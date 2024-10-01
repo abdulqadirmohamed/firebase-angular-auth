dd2d1c26ecc9cbe3e9d5374fb1cfa87ff43f784c
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-10-01 12:12:12 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firebase-angular-auth';
+
+  authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe(
+      (user: { email: any; displayName: any }) => {
+        if (user) {
+          this.authService.currentUserSignal.set({
+            email: user.email!,
+            username: user.displayName!,
+          });
+        }else{
+          this.authService.currentUserSignal.set(null)
+        }
+      }
+    );
+  }
 }
